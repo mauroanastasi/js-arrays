6ba538fe6eb5db68fb4e455b26b98bd6b718cbc1
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-19 15:46:47 +0100

    [UPD 3 es

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -17,10 +17,12 @@ console.log(fourthTeacher)
 teachers[4] =`Patrick`
 console.log(teachers[4]);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher)
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
